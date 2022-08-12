647537366dcf9db7edefddd6ffd22395a7bd83f6
Toshinori Sugita <s.toshi0607@gmail.com>
2022-08-12 14:21:28 +0900

    Add collectors for go-chi

---
+++ middleware.go
@@ -0,0 +1,77 @@
+package chiprometheus
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	defaultBuckets = []float64{300, 1200, 5000}
+)
+
+const (
+	RequestsCollectorName = "chi_requests_total"
+	LatencyCollectorName  = "chi_request_duration_milliseconds"
+)
+
+// Middleware is a handler that exposes prometheus metrics for the number of requests,
+// the latency, and the response size partitioned by status code, method, and HTTP path.
+type Middleware struct {
+	requests *prometheus.CounterVec
+	latency  *prometheus.HistogramVec
+}
+
+// New returns a new prometheus middleware for the provided service name.
+func New(name string) *Middleware {
+	var m Middleware
+	m.requests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:        RequestsCollectorName,
+			Help:        "Number of HTTP requests partitioned by status code, method and HTTP path.",
+			ConstLabels: prometheus.Labels{"service": name},
+		}, []string{"code", "method", "path"})
+
+	m.latency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        LatencyCollectorName,
+		Help:        "Time spent on the request partitioned by status code, method and HTTP path.",
+		ConstLabels: prometheus.Labels{"service": name},
+		Buckets:     defaultBuckets,
+	}, []string{"code", "method", "path"})
+
+	return &m
+}
+
+// Handler returns a handler for the middleware pattern.
+func (m Middleware) Handler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+		next.ServeHTTP(ww, r)
+
+		rp := chi.RouteContext(r.Context()).RoutePattern()
+		since := float64(time.Since(start).Milliseconds())
+		m.requests.WithLabelValues(http.StatusText(ww.Status()), r.Method, rp).Inc()
+		m.latency.WithLabelValues(http.StatusText(ww.Status()), r.Method, rp).Observe(since)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// Collectors returns collector for your own collector registry.
+func (m Middleware) Collectors() []prometheus.Collector {
+	return []prometheus.Collector{m.requests, m.latency}
+}
+
+// MustRegisterDefault registers collectors to DefaultRegisterer. If you don't initialize the collector registry,
+// this method should be called before calling promhttp.Handler().
+func (m Middleware) MustRegisterDefault() {
+	if m.requests == nil || m.latency == nil {
+		panic("collectors must be set")
+	}
+	prometheus.MustRegister(m.requests)
+	prometheus.MustRegister(m.latency)
+}
---
+++ middleware_test.go
@@ -0,0 +1,177 @@
+package chiprometheus_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	chiprometheus "toshi0607/chi-prometheus"
+)
+
+const testHost = "http://localhost"
+
+func TestMiddleware_MustRegisterDefault(t *testing.T) {
+	t.Run("must panic without collectors", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("must have panicked")
+			}
+		}()
+		m := chiprometheus.Middleware{}
+		m.MustRegisterDefault()
+	})
+
+	t.Run("must not panic with collectors", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("must not have panicked")
+			}
+		}()
+		m := chiprometheus.New("test")
+		m.MustRegisterDefault()
+		t.Cleanup(func() {
+			for _, c := range m.Collectors() {
+				prometheus.Unregister(c)
+			}
+		})
+	})
+}
+
+func TestMiddleware_Collectors(t *testing.T) {
+	m := chiprometheus.New("test")
+	want := 2
+	got := len(m.Collectors())
+	if got != want {
+		t.Errorf("number of collectors should be %v, got %v", want, got)
+	}
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestMiddleware_Handler(t *testing.T) {
+	r := chi.NewRouter()
+	m := chiprometheus.New("test")
+	m.MustRegisterDefault()
+	t.Cleanup(func() {
+		for _, c := range m.Collectors() {
+			prometheus.Unregister(c)
+		}
+	})
+	r.Use(m.Handler)
+	r.Handle("/metrics", promhttp.Handler())
+	r.Get(`/healthz`, testHandler)
+	r.Get(`/users/{firstName}`, testHandler)
+
+	paths := [][]string{
+		{"healthz"},
+		{"users", "bob"},
+		{"users", "alice"},
+		{"metrics"},
+	}
+	rec := httptest.NewRecorder()
+	for _, p := range paths {
+		u, err := url.JoinPath(testHost, p...)
+		if err != nil {
+			t.Error(err)
+		}
+		req, err := http.NewRequest("GET", u, nil)
+		if err != nil {
+			t.Error(err)
+		}
+		r.ServeHTTP(rec, req)
+	}
+	body := rec.Body.String()
+
+	if !strings.Contains(body, chiprometheus.RequestsCollectorName) {
+		t.Errorf("body should contain request total entry '%s'", chiprometheus.RequestsCollectorName)
+	}
+	if !strings.Contains(body, chiprometheus.LatencyCollectorName) {
+		t.Errorf("body should contain request duration entry '%s'", chiprometheus.LatencyCollectorName)
+	}
+
+	healthzCount := `chi_request_duration_milliseconds_count{code="OK",method="GET",path="/healthz",service="test"} 1`
+	bobCount := `chi_request_duration_milliseconds_count{code="OK",method="GET",path="/users/bob",service="test"} 1`
+	aliceCount := `chi_request_duration_milliseconds_count{code="OK",method="GET",path="/users/alice",service="test"} 1`
+	aggregatedCount := `chi_request_duration_milliseconds_count{code="OK",method="GET",path="/users/{firstName}",service="test"} 2`
+	if !strings.Contains(body, healthzCount) {
+		t.Errorf("body should contain healthz count summary '%s'", healthzCount)
+	}
+	if strings.Contains(body, bobCount) {
+		t.Errorf("body should contain Bob count summary '%s'", bobCount)
+	}
+	if strings.Contains(body, aliceCount) {
+		t.Errorf("body should contain Alice count summary '%s'", aliceCount)
+	}
+	if !strings.Contains(body, aggregatedCount) {
+		t.Errorf("body should contain first name count summary '%s'", aggregatedCount)
+	}
+}
+
+func TestMiddleware_HandlerWithCustomRegistry(t *testing.T) {
+	r := chi.NewRouter()
+	reg := prometheus.NewRegistry()
+	if err := reg.Register(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{})); err != nil {
+		t.Error(err)
+	}
+	if err := reg.Register(collectors.NewGoCollector()); err != nil {
+		t.Error(err)
+	}
+	m := chiprometheus.New("test")
+	for _, c := range m.Collectors() {
+		if err := reg.Register(c); err != nil {
+			t.Error(err)
+		}
+	}
+	t.Cleanup(func() {
+		for _, c := range m.Collectors() {
+			prometheus.Unregister(c)
+		}
+	})
+	promh := promhttp.InstrumentMetricHandler(
+		reg, promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
+	)
+
+	r.Use(m.Handler)
+	r.Handle("/metrics", promh)
+	r.Get(`/healthz`, testHandler)
+
+	paths := [][]string{
+		{"healthz"},
+		{"metrics"},
+	}
+	rec := httptest.NewRecorder()
+	for _, p := range paths {
+		u, err := url.JoinPath(testHost, p...)
+		if err != nil {
+			t.Error(err)
+		}
+		req, err := http.NewRequest("GET", u, nil)
+		if err != nil {
+			t.Error(err)
+		}
+		r.ServeHTTP(rec, req)
+	}
+	body := rec.Body.String()
+
+	if !strings.Contains(body, chiprometheus.RequestsCollectorName) {
+		t.Errorf("body should contain request total entry '%s'", chiprometheus.RequestsCollectorName)
+	}
+	if !strings.Contains(body, chiprometheus.LatencyCollectorName) {
+		t.Errorf("body should contain request duration entry '%s'", chiprometheus.LatencyCollectorName)
+	}
+
+	if !strings.Contains(body, "promhttp_metric_handler_requests_total") {
+		t.Error("body should contain promhttp_metric_handler_requests_total from ProcessCollector")
+	}
+	if !strings.Contains(body, "go_goroutines") {
+		t.Errorf("body should contain Go runtime metrics from GoCollector")
+	}
+}
